cb5020d89b9819c52e29634cd854c300c28453f5
Shreyas Shandilya <shandilya.shreyas@gmail.com>
2020-06-24 12:29:40 +0530

    commit 4 osc control model

--- RoFish_cpg/RoFish_cpg.ino
+++ RoFish_cpg/RoFish_cpg.ino
@@ -33,12 +33,13 @@
 
 #define LED_PIN 13
 #define LPERIOD 10000L	// loop period time in us.
-#define N 3	// number of oscillators
+#define N 4	// number of oscillators
 #define T LPERIOD/1000000.0 // T=10ms.
 
-#define SERVO1_PIN 3	// towards head
-#define SERVO2_PIN 10	// middle
-#define SERVO3_PIN 11	// tail
+#define SERVO1_PIN 2
+#define SERVO2_PIN 3	
+#define SERVO3_PIN 4
+#define SERVO4_PIN 5	
 
 unsigned long nextLoop;
 char c;
@@ -50,36 +51,36 @@ bool forward=true;
 float ar=20; // rad/sec
 float ax=20; // rad/sec
 
-// control inputs
+// control inputs Control input is the tonic input required by oscillators to drive the oscillations
 float W=3.5, R=0.3, X=0; // common control inputs frequency/amplitude/offset of the joints
-float cW[N]={PI, PI, PI};	// OHMEGAi - desired frequency rad/s.
-float cR[N]={0.42, 0.31, 0.40};	// Ri - desired amplitude in radians.
-float cX[N]={0, 0, 0};			// Xi - desired offset in radians.
+float cW[N]={PI, PI, PI, PI};	// OHMEGAi - desired frequency rad/s.
+float cR[N]={PI/2, PI/2, PI/2, PI/2 };	// Ri - desired amplitude in radians.
+float cX[N]={0, 0, 0, 0};			// Xi - desired offset in radians.
 
 // states
-float sA[N]={0,0,0}; // phase
-float sr[N]={0,0,0}; // amplitude
-float sx[N]={0,0,0}; // offset
-float sA_new[N]={0,0,0}; // phase
-float sr_new[N]={0,0,0}; // amplitude
-float sx_new[N]={0,0,0}; // offset
+float sA[N]={0,0,0,0}; // phase
+float sr[N]={0,0,0,0}; // amplitude
+float sx[N]={0,0,0,0}; // offset
+float sA_new[N]={0,0,0,0}; // phase
+float sr_new[N]={0,0,0,0}; // amplitude
+float sx_new[N]={0,0,0,0}; // offset
 
 // derivatives
 float sA_d=0; // phase dot
-float sr_d[N]={0,0,0}; // amplitude dot
-float sx_d[N]={0,0,0}; // offset dot
-float sr_d_new[N]={0,0,0}; // amplitude dot
-float sx_d_new[N]={0,0,0}; // amplitude dot
+float sr_d[N]={0,0,0,0}; // amplitude dot
+float sx_d[N]={0,0,0,0}; // offset dot
+float sr_d_new[N]={0,0,0,0}; // amplitude dot
+float sx_d_new[N]={0,0,0,0}; // amplitude dot
 
 // second derivatives
 float sr_dd=0;
 float sx_dd=0;
-float out_deg[N]={0,0,0}; // output phase time vector
+float out_deg[N]={0,0,0,0}; // output phase time vector
 
 Servo servo1;
 Servo servo2;
 Servo servo3;
-
+Servo servo4;
 
 //The setup function is called once at startup of the sketch
 void setup()
@@ -89,43 +90,66 @@ void setup()
 	servo1.attach(SERVO1_PIN);
 	servo2.attach(SERVO2_PIN);
 	servo3.attach(SERVO3_PIN);
+    servo4.attach(SERVO4_PIN);
 	servo1.write(90); // turn servos to the center position
 	servo2.write(90);
 	servo3.write(90);
+    servo4.write(90);
 	delay(1500);
 
 	// init, 1/s coupling weights
 	w[1][1] = 0;
 	w[1][2] = 5;
 	w[1][3] = 5;
+  w[1][4] = 5;
 	w[2][1] = 5;
 	w[2][2] = 0;
 	w[2][3] = 5;
+  w[2][4] = 5;
 	w[3][1] = 5;
 	w[3][2] = 5;
 	w[3][3] = 0;
+  w[3][4] = 5;
+  w[4][1] = 5;
+  w[4][2] = 5;
+  w[4][3] = 5;
+  w[4][4] = 0;
 
 	// forward swimming, 1/s phase biases
 	f[1][1] = 0.0;
 	f[1][2] = -0.7;
 	f[1][3] = -1.41;
+  f[1][4] = -0.7;
 	f[2][1] = -f[1][2];
 	f[2][2] = 0.0;
 	f[2][3] = f[1][3] - f[1][2];
+  f[2][4] = f[1][4] - f[1][2];
 	f[3][1] = -f[1][3];
 	f[3][2] = -f[2][3];
 	f[3][3] = 0.0;
+  f[3][4] = f[1][3] - f[1][4];
+  f[4][1] = -f[1][4];
+  f[4][2] = -f[2][4];
+  f[4][3] = -f[3][4];
+  f[4][4] = 0.0;
 
 	// backward swimming, 1/s phase biases
 	b[1][1] = 0.0;
-	b[1][2] = 0.7;
-	b[1][3] = 1.41;
-	b[2][1] = -b[1][2];
-	b[2][2] = 0.0;
-	b[2][3] = b[1][3] - b[1][2];
-	b[3][1] = -b[1][3];
-	b[3][2] = -b[2][3];
-	b[3][3] = 0.0;
+    b[1][2] = 0.7;
+    b[1][3] = 1.41;
+    b[1][4] = 0.7;
+    b[2][1] = -b[1][2];
+    b[2][2] = 0.0;
+    b[2][3] = b[1][3] - b[1][2];
+    b[2][4] = b[1][4] - b[1][2];
+    b[3][1] = -b[1][3];
+    b[3][2] = -b[2][3];
+    b[3][3] = 0.0;
+    b[3][4] = b[1][3] - b[1][4];
+    b[4][1] = -b[1][4];
+    b[4][2] = -b[2][4];
+    b[3][4] = -b[4][3];
+    b[4][4] = 0.0;
 
 	nextLoop = micros() + LPERIOD; // Set the loop timer variable.
 }
@@ -175,58 +199,11 @@ for(i=0; i<N; i++){
 servo1.write( 90 + round(out_deg[0]) );
 servo2.write( 90 + round(out_deg[1]) );
 servo3.write( 90 + round(out_deg[2]) );
+servo4.write( 90 + round(out_deg[3]) );
 
-if(Serial.available()){
-	c = Serial.read();
-    if(c == 'q') {	// use the 'q' and 'a' keys to set the frequency of oscillation.
-        W += 0.5;
-        W = constrain(W, 0, 8);
-    	Serial.print("speed [rad/s]: ");
-    	Serial.println(W,2);
-    }
-    if(c == 'a') {
-        W -= 0.5;
-        W = constrain(W, 0, 8);
-    	Serial.print("speed [rad/s]: ");
-    	Serial.println(W,2);
-    }
-    if(c == 'w') { // use the 'w' and 's' keys to set the amplitude.
-        R += 0.05;
-        R = constrain(R, 0, 0.5);
-    	Serial.print("amplitude [rad]: ");
-    	Serial.println(R,2);
-    }
-    if(c == 's') {
-        R -= 0.05;
-        R = constrain(R, 0, 0.5);
-    	Serial.print("amplitude [rad]: ");
-    	Serial.println(R,2);
-    }
-    if(c == 'e') { // use the 'e' and 'd' keys to set the offset (steering control)
-        X += 0.05;
-        X = constrain(X, -0.5, 0.5);
-    	Serial.print("offset [rad]: ");
-    	Serial.println(X,2);
-    }
-    if(c == 'd') {
-        X -= 0.05;
-        X = constrain(X, -0.5, 0.5);
-    	Serial.print("offset [rad]: ");
-    	Serial.println(X,2);
-    }
-    if(c == 'f') { // swim forward
-    	forward = true;
-        Serial.println("forwards");
-    }
-    if(c == 'b') { // swim backward
-        	forward = false;
-            Serial.println("backwards");
-        }
-}
 digitalWrite(LED_PIN, false); //loop execution time, about 2ms.
 
 
 while(nextLoop > micros());  //wait until the end of the time interval
 	nextLoop += LPERIOD;  //set next loop time at current time + LOOP_PERIOD
 }
-
